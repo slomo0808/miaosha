268b96e9cef52f0ac319d74170bbec60995864ba
slomo0808 <944431128@qq.com>
2021-04-16 13:40:21 +0800

    秒杀接口优化

---
+++ src/main/java/top/slomo/miaosha/rabbitmq/MiaoshaMessage.java
@@ -0,0 +1,8 @@
+package top.slomo.miaosha.rabbitmq;/**
+ * @description: .
+ *
+ * @date: 2021-04-16
+ * @author: YuBo
+ */
+public class MiaoshaMessage {
+}
---
+++ src/main/java/top/slomo/miaosha/redis/MiaoshaKeyPrefix.java
@@ -0,0 +1,8 @@
+package top.slomo.miaosha.redis;/**
+ * @description: .
+ *
+ * @date: 2021-04-16
+ * @author: YuBo
+ */
+public class MiaoshaKeyPrefix {
+}
